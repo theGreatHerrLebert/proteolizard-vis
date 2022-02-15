ab1cc3098c8d02c57aa37778970e4b8f728cdaf9
theGreatHerrLebert <davidteschner@googlemail.com>
2022-02-15 15:43:47 +0100

    fixing

--- pyproteolizardvis/cluster.py
+++ pyproteolizardvis/cluster.py
@@ -2,8 +2,6 @@ import abc
 import pandas as pd
 from abc import ABC
 
-from ipywidgets import widgets
-
 import hdbscan
 
 from sklearn.metrics import pairwise
@@ -14,53 +12,7 @@ import plotly.express as px
 import numpy as np
 from ipywidgets import widgets
 from plotly.subplots import make_subplots
-
-
-def calculate_statistics(clusters, noise):
-    sum_intensity_clusters = clusters.groupby(['label'])['intensity'].sum().sum()
-    sum_intensity_noise = noise.groupby(['label'])['intensity'].sum().sum()
-
-    intensity_ratio = np.round(sum_intensity_clusters / sum_intensity_noise, 3)
-
-    num_points_clusters = clusters.shape[0]
-    num_points_noise = noise.shape[0]
-
-    points_ratio = np.round(num_points_clusters / num_points_noise, 3)
-
-    num_clusters = int(clusters.label.max())
-
-    table_dict = {'Total': [num_points_clusters + num_points_noise,
-                            sum_intensity_clusters + sum_intensity_noise, num_clusters],
-                  'Cluster': [num_points_clusters, sum_intensity_clusters, num_clusters],
-                  'Noise': [num_points_noise, sum_intensity_noise, 0],
-                  'Ratio': [points_ratio, intensity_ratio, 0]}
-
-    summary_table = pd.DataFrame(table_dict, index=['Points', 'Intensity', 'Clusters'])
-
-    sum_table = clusters.groupby('label')
-
-    return summary_table, sum_table
-
-def get_discrete_color_swatches():
-    """
-    """
-    return list(filter(lambda x: x.find('_') == -1, [attr for attr in dir(px.colors.qualitative)]))
-
-
-def get_initial_histogram():
-    """
-    helper for generation of template vis, can be used for init or custom widget inspection
-    """
-
-    # create 4 histograms: cluster rt distribution, scan distibution, mz distribution, size distribution
-    fig = make_subplots(rows=2, cols=2, subplot_titles=("Cycle", "Scan", "m/z", "Size"))
-    fig.add_trace(go.Histogram(x=np.random.normal(size=1000)), row=1, col=1)
-    fig.add_trace(go.Histogram(x=np.random.normal(size=1000)), row=1, col=2)
-    fig.add_trace(go.Histogram(x=np.random.normal(size=1000)), row=2, col=1)
-    fig.add_trace(go.Histogram(x=np.random.normal(size=1000)), row=2, col=2)
-    fig.update_layout(title_text="Cluster distributions", showlegend=False,
-                      template="plotly_white")
-    return fig
+from proteolizardvis.utility import calculate_statistics, get_initial_histogram
 
 
 class ClusterVisualizer(abc.ABC):
--- pyproteolizardvis/filter.py
+++ pyproteolizardvis/filter.py
@@ -16,11 +16,11 @@ class MassSpecDataFilter(abc.ABC):
         self.mz_max = widgets.BoundedFloatText(value=510.0, min=1.0, max=2500.0, step=.1, description='m/z max:',
                                                disabled=False)
         # intensity controls
-        self.intensity_min = widgets.IntSlider(value=200, min=0, max=500, step=10, description='Min intensity:',
+        self.intensity_min = widgets.IntSlider(value=150, min=0, max=1000, step=10, description='Min intensity:',
                                                continuous_update=False)
 
         # filter button
-        self.filter_button = widgets.Button(description="Filter Precursors")
+        self.filter_button = widgets.Button(description="Filter")
         self.filter_button.on_click(self.on_filter_clicked)
         self.filter_controls = widgets.HBox(children=[self.mz_min, self.mz_max, self.intensity_min])
         self.controls = widgets.VBox(children=[self.filter_controls, self.filter_button])
